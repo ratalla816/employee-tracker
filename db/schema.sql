d3a4cf41989d7b1eaba2fbcbc328ddcc746f1f38
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-13 12:26:42 -0500

    seeds.sql error

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,6 @@
- DROP TABLE IF EXISTS employees;
- DROP TABLE IF EXISTS roles;
  DROP TABLE IF EXISTS divisions;
-
-
-
-CREATE TABLE employees (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  manager_id INTEGER NULL,
-  roles_id INTEGER NOT NULL,
-  FOREIGN KEY (manager_id) REFERENCES employee(id),
-  FOREIGN KEY (role_id) REFERENCES roles(id)
-   -- CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-);
-
-CREATE TABLE roles (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(11,2) NOT NULL,
-  division_id INTEGER(50) NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(division_id) REFERENCES division(id)
- );
-
+ DROP TABLE IF EXISTS roles;
+ DROP TABLE IF EXISTS employees;
 
 
 CREATE TABLE divisions (
@@ -33,14 +10,28 @@ CREATE TABLE divisions (
   description TEXT
 );
 
--- CREATE TABLE managers (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30) NOT NULL,
---   last_name VARCHAR(30) NOT NULL,
-  -- party_id INTEGER,
-  -- industry_connected BOOLEAN NOT NULL,
-  -- CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
--- );
+CREATE TABLE roles (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(11,2) NOT NULL,
+  divisions_id INTEGER NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(divisions_id) REFERENCES divisions(id)
+ );
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  roles_id INTEGER NULL,
+  manager_id INTEGER NULL,
+  FOREIGN KEY (roles_id) REFERENCES roles(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
+ );
+
+
+
+
 
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,34 @@
- DROP TABLE IF EXISTS employees;
- DROP TABLE IF EXISTS roles;
- DROP TABLE IF EXISTS divisions;
+ USE employee_db;
  
- INSERT INTO employees (first_name, last_name, manager_id, roles_id)
- VALUES
-  ('Rob', 'Atalla', 1, 1,),
-  ('Jack', 'London', 2,3),
-  ('Robert', 'Bruce', 2,2),
-  ('Peter', 'Greenaway', 2,4),
-
-INSERT INTO roles (title, salary, division_id)
+ 
+ INSERT INTO divisions (division)
 VALUES
-  ('manager', 100000000, 1,),
-  ('employee', 150000, 2),
+  ("csuite"),
+  ("sales"),
+  ("research"),
+  ("devops")
+
+ INSERT INTO roles (title, salary, division_id)
+VALUES
+  ("manager", 10000000,1),
+  ("employee", 200000,2),
+  ("employee", 200000,3),
+  ("employee", 200000,4)
+  
+ INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+ VALUES
+  ("Rob", "Atalla",1,NULL),
+  ("Jack", "London",2,1),
+  ("Robert", "Bruce",3,1),
+  ("Peter", "Greenaway",4,1)
+
+  -- ("Rob", "Atalla"),
+  -- ("Jack", "London"),
+  -- ("Robert", "Bruce"),
+  -- ("Peter", "Greenaway")
+
   
 
-INSERT INTO divisions (name)
-VALUES
-  ('c-suite'),
-  ('sales'),
-  ('research'),
-  ('devops')
+
   
   
