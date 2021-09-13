04f95176906a73c1978f341cfd41919c915fb828
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-13 00:18:54 -0500

    updated schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,39 +1,40 @@
-DROP TABLE IF EXISTS votes;
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
 
-CREATE TABLE candidates (
+
+CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  -- party_id INTEGER,
+  -- industry_connected BOOLEAN NOT NULL,
+  -- CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 
-CREATE TABLE voters (
+CREATE TABLE roles (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  -- created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE votes (
+CREATE TABLE managers (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  -- With ON DELETE CASCADE, deleting the reference key will also delete the entire row from this table.
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  -- party_id INTEGER,
+  -- industry_connected BOOLEAN NOT NULL,
+  -- CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
+CREATE TABLE departments (
+   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO parties (name, description)
+INSERT INTO departments (name, description)
 VALUES
-  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
-  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
-  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
+  (''),
+  (''),
+  ('');
 
 INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
 VALUES
@@ -69,4 +69,14 @@ VALUES
   ('Hubert', 'Crackanthorpe', 'hcrackan@goodletters.com'),
   ('William', 'Carleton', 'wcarleton@literature.com'),
   ('Gerald', 'Griffin', 'ggriff@lit.net');
+  ('Ronald', 'Firbank', ),
+  ('Virginia', 'Woolf', ),
+  ('Piers', 'Gaveston', ),
+  ('Charles', 'LeRoi', ),
+  ('Katherine', 'Mansfield',),
+  ('Dora', 'Carrington', ),
+  ('Edward', 'Bellamy', ),
+  ('Montague', 'Summers', ),
+  ('Octavia', 'Butler', ),
+  ('Unica', 'Zurn', );
   
