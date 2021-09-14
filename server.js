62888af15e800d475730f3e0d29863eb18cdd27a
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-14 13:00:26 -0500

    comment out anything related to express

--- server.js
+++ server.js
@@ -12,25 +12,25 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
+// // Use apiRoutes
+// app.use('/api', apiRoutes);
  
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
 
-// Start server after DB connection
-db.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  });
+// // Start server after DB connection
+// db.connect(err => {
+//     if (err) throw err;
+//     console.log('Database connected.');
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   });
 
   // --------------------------------------------------------- //
 
