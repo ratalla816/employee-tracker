4de2f6110dcdbba278a4aaefef4820c941d6d3bd
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-13 23:47:34 -0500

    deleted db.sql and connection.js

--- server.js
+++ server.js
@@ -32,6 +32,25 @@ db.connect(err => {
 
   // --------------------------------------------------------- //
 
+  // pulled from connection.js
+  const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'root',
+      database: 'employee_db'
+    },
+    console.log('Connected to the employee database.')
+  );
+
+  // --------------------------------------------------------- //
+// pulled from the apiRoute files
+
   // division routes //
   const express = require('express');
 const router = express.Router();
