3bdb1e01ca0f035b9eae624bd0d818e1de23fc33
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-14 23:40:54 -0500

    improved for loop

--- server.js
+++ server.js
@@ -44,181 +44,17 @@ const db = mysql.createConnection(
           'view / edit managers',
           'exit program',
       ]
-  },    
-
-  {
-    type: "input",
-    name: "last_name",
-    message: "Please enter the manager's last name.",
-  },    
-      
-  {
-      type: "list",
-      name: "department_id",
-      message: "Please enter the manager's department.",
-      choices: [
-        "executive",
-        "sales",
-        "research",
-        "accounting",
-        "devops",
-        "compliance",
-        "hr"
-      ],
-  },
-*** ]) ****
-  {
-      type: "input",
-      name: "employee(id)",
-      message: "Please enter the manager's employee id.",
-  },
-      
-  {
-      type: "input",
-      name: "roles_id",
-      message: "Please enter the manager's role id.",
-  }
+  }    
+  
+]) 
+  
 }
-    
-  // end of question array //
-  
-  const employArray = [];
-  
-      
-       const buildManager = () => {
-       return inquirer.prompt(managerPrompts)
-      .then((prompts) => {
-          const manager = new Manager(prompts.first_name, prompts.last_name, prompts.department_id, prompts.employee(id), prompts.roles_id);
-          employArray.push(manager);
-          console.log(manager)
-           // 
-                  
-      })  
-  };
-  
-  const buildEmployee = () => {
-      
-      return inquirer.prompt ([
-          {
-             type: 'input',
-             name: 'first_name',
-             message: "Please enter the employee's first name", 
-          },
-      
-          {
-             type: 'input',
-             name: 'last_name',
-             message: "Please enter the employee's last name", 
-          },
-      
-          {
-              type: 'list',
-              name: 'roles_id',
-              message: "Please select their role",
-              choices: ['accountexec', 'scientist', 'accountant', 'engineer', 'attorney', 'recruiter']
-          },
-           
-          {
-              type: 'input',
-              name: 'manager_id',
-              message: "Please enter their manager's ID (if unknown, enter 0).",
-          },
-  
-          {
-              type: 'confirm',
-              name: 'buildEmployeeConfirm',
-              message: 'Enter another employee?',
-          }
-      ])
-      .then(userInput => {
-          // Selects the data sets for each employee class
-  
-          let { first_name, last_name, roles_id, manager_id, buildEmployeeConfirm } = userInput; 
-          let employee; 
-  
-          if (roles_id === "accountexec") {
-              employee = new Engineer (first_name, last_name, roles_id, manager_id);
-  
-           } else if (roles_id === "scientist") {
-              employee = new Intern (first_name, last_name, roles_id, manager_id);
-
-           } else if (roles_id === "accountant") {
-              employee = new Intern (first_name, last_name, roles_id, manager_id);  
-
-           } else if (roles_id === "engineer") {
-              employee = new Intern (first_name, last_name, roles_id, manager_id);  
-              
-           } else if (roles_id === "attorney") {
-              employee = new Intern (first_name, last_name, roles_id, manager_id);   
-
-           } else if (roles_id === "recruiter") {
-              employee = new Intern (first_name, last_name, roles_id, manager_id);  
-           }
-          // pushes newly built employee to employArray
-          employArray.push(employee); 
-  
-          if (buildEmployeeConfirm) {
-              return buildEmployee(employArray); 
-          } else {
-              return employArray;
-          }
-      })
-  
-  };
-
-
-  
-  
-  // If user chooses not to buildEmployee then data is pushed to DB 
-  const sql = data => {
-    const params = [req.params.id];
-      fs.post('/employee/:id', data, err => {
-            if (err) {
-              console.log(err);
-              return;
-          } else {
-              console.log('Updated database, result "OK"')
-          }
-      })
-  }; 
-  
-  buildManager()
-  // callback to execute when the Promise is resolved.
-    .then(buildEmployee)
-    .then(employArray => {
-  // callback to execute when the Promise is rejected.      
-      return renderDB(employArray);
-    })
-    .then(finalDB => {
-   // Promise for the completion of which ever callback is executed     
-      return updateDB(finalDB);
-    })
-    .catch(err => {
-   console.log(err);
-    });
-  
- 
- 
- 
- 
-  // --------------------------------------------------------- //
-// pulled from the apiRoute files I deleted earlier
-
-  // department routes //
-//   const express = require('express');
-// const app = express.app();
-// const db = require('../../db/connection');
-
-// GET all department //
-
-
-  // end of role routes //
-
-  generateDB = (data) => {
-
-    profileArray = []; 
-
-    for (let i = 0; i < data.length; i++) {
+    // end of question array //      
+   
+    .then((userInput) => {
+       return inquirer.prompt(inquirerPrompts)
+                         
+       for (let i = 0; i < data.length; i++) {
         const employee = data[i];
         const role = employee.getRole(); 
 
@@ -258,17 +94,90 @@ const db = mysql.createConnection(
         }
     }
 
+      .then(userInput => {
+          // Selects the data sets for each employee class
+  
+          let { first_name, last_name, roles_id, manager_id, buildEmployeeConfirm } = userInput; 
+          let employee; 
+  
+          if (roles_id === "accountexec") {
+              employee = new Engineer (first_name, last_name, roles_id, manager_id);
+  
+           } else if (roles_id === "scientist") {
+              employee = new Intern (first_name, last_name, roles_id, manager_id);
+
+           } else if (roles_id === "accountant") {
+              employee = new Intern (first_name, last_name, roles_id, manager_id);  
+
+           } else if (roles_id === "engineer") {
+              employee = new Intern (first_name, last_name, roles_id, manager_id);  
+              
+           } else if (roles_id === "attorney") {
+              employee = new Intern (first_name, last_name, roles_id, manager_id);   
+
+           } else if (roles_id === "recruiter") {
+              employee = new Intern (first_name, last_name, roles_id, manager_id);  
+           }
+          // pushes newly built employee to employArray
+          employArray.push(employee); 
+  
+          if (buildEmployeeConfirm) {
+              return buildEmployee(employArray); 
+          } else {
+              return employArray;
+          }
+      })
+  
+  
+
+
+  
+  
+  // If user chooses not to buildEmployee then data is pushed to DB 
+  const sql = data => {
+    const params = [req.params.id];
+      fs.post('/employee/:id', data, err => {
+            if (err) {
+              console.log(err);
+              return;
+          } else {
+              console.log('Updated database, result "OK"')
+          }
+      })
+  }; 
+  
+  buildManager()
+  // callback to execute when the Promise is resolved.
+    .then(buildEmployee)
+    .then(employArray => {
+  // callback to execute when the Promise is rejected.      
+      return renderDB(employArray);
+    })
+    .then(finalDB => {
+   // Promise for the completion of which ever callback is executed     
+      return updateDB(finalDB);
+    })
+    .catch(err => {
+   console.log(err);
+    });
+  
+ 
+ 
+ 
+ 
+ 
+
+// GET all department //
+
+
+  // end of role routes //
+
+  
     
-    // Records in the array are joined so the data can be rendered in the DB
-    const employeeRecords = profileArray.join('')
+   
 
-    const renderEmployees = renderEmployeesTable(employeeRecords); 
-    return renderEmployees;
 
-}
 
-// creating the index file with all the teammembers' Records 
-const renderDB = function (employeeRecords) {   
 
 
   
